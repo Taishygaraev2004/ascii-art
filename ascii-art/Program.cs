5b2dd936f5a95200e5893fa27887cd888e36cb73
Taishygaraev2004 <taishygaraev.aidyn@gmail.com>
2025-08-16 18:49:15 +0500

    alert argument

--- ascii-art/Program.cs
+++ ascii-art/Program.cs
@@ -8,65 +8,76 @@ class Program
     {
     static void Main(string[] args)
     {
-        string str="";
-        for (int i = 0; i < args.Length; i++)
+        string str = "";
+        if (args != null && args.Length > 0)
         {
-            str += args[i]+" ";
-           
-        }
-        Console.WriteLine(str);
-      
-        string filepath = "C:\\Users\\user\\source\\repos\\ascii-art\\ascii-art\\standard.txt";
-       
-
-        using (StreamReader reader = new StreamReader(filepath)) {
-
-            string line;
-            Dictionary<char, string[] > asciiMap = new Dictionary<char, string[]>();
-            List<string> value = new List<string>();
-            int i = 0;
-            char j = (char)32;
-            while ((line = reader.ReadLine()) != null)
+            for (int i = 0; i < args.Length; i++)
             {
-                
-                if (string.IsNullOrEmpty(line)){
-                    continue;
-                }
-                else
-                {
-                    i ++;
-                }
-                value.Add(line);
-                if (i == 8)
-                {
-                    
-                    i = 0;
-                    asciiMap[j] = value.ToArray();
-                    value = new List<string>();
-                    j++;
-                }
-                    
+                str += args[i] + " ";
 
             }
-            // string dictionary = "Hello I Am Aidyn";
-            int l=asciiMap.First().Value.Length;
-            
-            for (i = 0; i < l; i++)
+            Console.WriteLine(str);
+
+            string filepath = "C:\\Users\\user\\source\\repos\\ascii-art\\ascii-art\\standard.txt";
+
+
+            using (StreamReader reader = new StreamReader(filepath))
             {
-                foreach (char c in str)
+
+                string line;
+                Dictionary<char, string[]> asciiMap = new Dictionary<char, string[]>();
+                List<string> value = new List<string>();
+                int i = 0;
+                char j = (char)32;
+                while ((line = reader.ReadLine()) != null)
                 {
-                    if (asciiMap.ContainsKey(c))
+
+                    if (string.IsNullOrEmpty(line))
                     {
-                      
-    
-                        Console.Write(asciiMap[c][i]);
-                        
+                        continue;
                     }
-                }
-                Console.WriteLine();
-            }
-        }
+                    else
+                    {
+                        i++;
+                    }
+                    value.Add(line);
+                    if (i == 8)
+                    {
 
-            
+                        i = 0;
+                        asciiMap[j] = value.ToArray();
+                        value = new List<string>();
+                        j++;
+                    }
+
+
+                }
+                // string dictionary = "Hello I Am Aidyn";
+                int l = asciiMap.First().Value.Length;
+
+                for (i = 0; i < l; i++)
+                {
+                    foreach (char c in str)
+                    {
+                        if (asciiMap.ContainsKey(c))
+                        {
+
+
+                            Console.Write(asciiMap[c][i]);
+
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+
+        }
+        else
+        {
+            Console.WriteLine("Введите аргумент для компиляций");
+            return;
+        }
     }
-}
+
+    }
